52f751941075d85291263b1d2a4685baca1f44ec
Spandana20 <30945662+Spandana20@users.noreply.github.com>
2017-09-16 20:32:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ function createTemplate(data){
     //var heading = data.heading;
    // var date = data.date;
     //var content= data.content;
-    //var commomHtmlTemplate =
+    var commomHtmlTemplate =
         `<html>
         <head>
             <title>
