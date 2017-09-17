051af7ae647edaf5a4cf8ce487a1f3eb048e7bdc
Spandana20 <30945662+Spandana20@users.noreply.github.com>
2017-09-17 09:21:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,6 +106,9 @@ app.get('/:articleName', function (req, res) {
     res.send(createTemplate(articles[articleName]));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
