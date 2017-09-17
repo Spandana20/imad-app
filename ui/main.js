a23fef11a7b305656b30f0b24b42cbc6b68ebeff
Spandana20 <30945662+Spandana20@users.noreply.github.com>
2017-09-17 09:39:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,16 @@
 console.log('Loaded!');
+
+//Changing the existing string to new value
 var element = document.getElementById("mainText");
 element.innerHTML = 'New Value';
+
+//Moving the image
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 1;
+    image.style.marginLeft = marginLeft + 'px';
+}
+var image = document.getElementById("maddy");
+image.onclick = function(){
+    var interval = setInterval(moveRight, 100);
+}
