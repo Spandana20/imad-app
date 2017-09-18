5bc6d47860a7b55a073c97da49bbf09459cf26d0
Spandana20 <30945662+Spandana20@users.noreply.github.com>
2017-09-18 10:50:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,7 +16,7 @@ image.onclick = function(){
 }*/
 var button = document.getElementById('counter');
 var counter = 0;
-button.onClick = function(){
+button.onclick = function(){
     counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
