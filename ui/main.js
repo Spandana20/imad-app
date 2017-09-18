2598ba717bb9d29b4e37601f4f87fe819380dcad
Spandana20 <30945662+Spandana20@users.noreply.github.com>
2017-09-18 11:17:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,10 +14,30 @@ var image = document.getElementById("maddy");
 image.onclick = function(){
     var interval = setInterval(moveRight, 100);
 }*/
+
+
+//Counter code to increment
 var button = document.getElementById('counter');
 var counter = 0;
+
 button.onclick = function(){
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    //Make a request
+    var request = new XMLHttpRequest();
+
+    //Capture the request
+    request.onreadystatechange = function(){
+        if(request.readystate === XMLHttpRequest.DONE){
+            if(request.status === 200){
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+         }
+        }
+    };
+    
+    //Make the request
+    request.open('GET', 'http://spandanapidishetty.imad.hasura-app.io/',true);
+    request.send(null);
 };
+
+
